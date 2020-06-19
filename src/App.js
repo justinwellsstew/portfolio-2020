d7a46cd546f82a3f1a49db97970c3a5eeab45de1
Justin Stewart <stewartjustin3@gmail.com>
2020-06-19 16:49:18 -0400

    basic homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    render(){
+        return (
+            <header class="header"> 
+                <div class="header__text-box"> 
+                    <h1>
+                        <span class="header__main-header">Justin Stewart</span>
+                        <span class="header__sub-header">Web/Print portfolio</span>
+                    </h1>
+                    <a href="#" class="btn btn--black">explore</a>
+                </div>
+            </header>
+        )
+        }
 }
 
 export default App;
